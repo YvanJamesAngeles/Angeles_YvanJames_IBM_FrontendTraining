83c31e7711df322e2201ab6eeb415fbe6ee89558
Yvan James Angeles <yvan.james.angeles@ibm.com>
2026-06-16 11:09:44 +0800

    Day 1

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
 <script setup lang="ts">
 // import HelloWorld from './components/HelloWorld.vue'
-import TaskCounter from './components/TaskCounter.vue';
+import TaskCounter from './views/day1/TaskCounter.vue';
 
 </script>
 
 <template>
   <!-- <HelloWorld /> -->
-   
   <TaskCounter />
 </template>
